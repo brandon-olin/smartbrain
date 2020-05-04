539340d1cd341a62ca9007a18523976c94419055
Brandon Olin <olin.brandon@gmail.com>
2020-05-04 17:08:09 -0400

    front end mostly completed, saving progress EOD

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/Navigation.js';
 import Logo from './components/logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 200,
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Particles className='particles' params={particlesOptions} />      
         <Navigation/>
         <Logo/>
+        <Rank/>
         <ImageLinkForm/>
         {/*<FaceRecognition/>*/}
       </div>
