4b62bbb1a0b3732aa651550212e6e0ce1d12075d
Brandon Olin <olin.brandon@gmail.com>
2020-05-07 12:47:36 -0400

    'deploy-to-heroku'

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3001/imageurl', {
+      fetch('https://shielded-anchorage-26190.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -86,7 +86,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch('https://shielded-anchorage-26190.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
